3bc04aeb0f056bbc4f602ab0e5da769a55b71799
M-Samii <mahmoud.samy.ms18@gmail.com>
2025-07-23 12:16:20 +0300

    tesst weebhook

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello Eyego' });
+  res.json({ message: 'Hello Eyego from samii' });
 });
 
 app.listen(port, () => {
